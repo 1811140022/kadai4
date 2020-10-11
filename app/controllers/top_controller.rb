c726cd921c268be2cb6b01a3d0c4a24a9ec7223b
Daiki Moriuchi <1811140022g@ed.fuk.kindai.ac.jp>
2020-10-11 15:31:09 +0000

    修正

---
+++ app/controllers/top_controller.rb
@@ -0,0 +1,37 @@
+class TopController < ApplicationController
+  
+  def main
+    if session[:login_uid]
+      session.delete(:login_uid)
+      render :main
+    else
+      render :login
+    end
+  end
+
+  def login
+    if user(params[:uid], params[:pass]) then
+      session[:login_uid] = params[:uid]
+      redirect_to root_path
+    else
+      render :error
+    end
+  end
+  
+  def user(id, pass)
+        aaa = User.find_by(uid: id)
+        if aaa.nil? then
+            return false
+        end
+        
+        if aaa [:pass] == pass then
+            return true
+        end
+        return false
+  end
+  def logout
+    session.delete(:login_uid)
+    redirect_to root_path
+
+  end
+end
